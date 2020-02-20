b0d6a1b59b53ba9744f1769f65eb11c226db420c
Lorenzo Rivaroli <lorenzo.rivaroli@zupit.it>
2020-02-20 19:53:18 +0100

    templates

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from django.shortcuts import render
+
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello world</h1>')
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
